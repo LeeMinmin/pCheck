c29bfffc40872c23c7c88ebea3abd220dd5ebd08
liminmin <liminmin@youmi.net>
2014-09-22 18:07:09 +0800

    new code

--- src/com/pCheck/activity/MainActivity.java
+++ src/com/pCheck/activity/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.example.pcheck.R;
 import com.pCheck.util.AppInfo;
+import com.pCheck.util.ViewHolder;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
@@ -12,6 +13,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,6 +35,7 @@ public class MainActivity extends ActionBarActivity {
 	private ListView appListView;
 	private Button startTest;
 	private boolean isRadioChecked = false;
+	private int tempPositon = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,7 @@ public class MainActivity extends ActionBarActivity {
 			}
 		});
 		appListView.setAdapter(new ListAdapter(this));
+		appListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 	}
 
 	@Override
@@ -69,23 +73,27 @@ public class MainActivity extends ActionBarActivity {
 	
 	
 	private class ListAdapter extends BaseAdapter{
-		View[] itemViews;		
+		List<View> itemViews;
+		LayoutInflater mInflater;
 		public ListAdapter(Context context){
 			//获取手机内非系统预装的应用程序列表
+			this.mInflater = LayoutInflater.from(context);
 			PackageManager pManager = context.getPackageManager();
 			List<PackageInfo> paklist = pManager.getInstalledPackages(0);
 			int len = paklist.size();
+			//appInfo = new ArrayList();
 			for (int i=0; i<len; i++){
 				PackageInfo pak = (PackageInfo) paklist.get(i);
 				if ((pak.applicationInfo.flags & pak.applicationInfo.FLAG_SYSTEM) <= 0){
-					String text = pManager.getApplicationLabel(pak.applicationInfo).toString();
-					String pname = pak.applicationInfo.packageName;
-					Drawable img = pManager.getApplicationIcon(pak.applicationInfo);
-					itemViews[i] = makeItemView(img, text);
+					AppInfo app = new AppInfo();
+					app.setAppText(pManager.getApplicationLabel(pak.applicationInfo).toString());
+					app.setAppImg(pManager.getApplicationIcon(pak.applicationInfo));
+					app.setAppPackage(pak.applicationInfo.packageName);
+					appInfo.add(app);
 				}
 			}
 		}
-	
+		/*
 		private View makeItemView(Drawable resImg, String text){
 			LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
 			View item = inflater.inflate(R.layout.list_app, null);
@@ -97,36 +105,59 @@ public class MainActivity extends ActionBarActivity {
 			rb.setOnCheckedChangeListener(new OnCheckedChangeListener(){
 				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
 					if(isChecked){
-						isRadioChecked = true;
+						isRadioChecked = true;						
 					}
 				}
 			});
 			return item;
 		}
-
+		*/
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
-			return itemViews.length;
+			//Log.d("itemViews length", itemViews.size()+"");		
+			return appInfo.size();
 		}
 
 		@Override
 		public Object getItem(int position) {
 			// TODO Auto-generated method stub
-			return itemViews[position];
+			//return itemViews.get(position);
+			return appInfo.get(position);
 		}
 
 		@Override
 		public long getItemId(int position) {
 			// TODO Auto-generated method stub
+			//return position;
 			return position;
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+		public View getView(final int position, View convertView, ViewGroup parent) {
 			// TODO Auto-generated method stub
-			if(convertView == null)
-				return itemViews[position];
+			ViewHolder holder;
+			if(convertView == null){
+				convertView = mInflater.inflate(R.layout.list_app, null);
+				holder = new ViewHolder();
+				holder.app_text = (TextView) convertView.findViewById(R.id.appText);
+				holder.app_img = (ImageView) convertView.findViewById(R.id.appImage);
+				holder.app_radio = (RadioButton) convertView.findViewById(R.id.rbutton);
+				convertView.setTag(holder);
+			}else{
+				holder = (ViewHolder)convertView.getTag();
+			}			
+			holder.app_text.setText(appInfo.get(position).getAppText());
+			holder.app_img.setImageDrawable(appInfo.get(position).getAppImg());
+			holder.app_radio.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+				@Override
+				public void onCheckedChanged(CompoundButton buttonView,
+						boolean isChecked) {
+					// TODO Auto-generated method stub
+					isRadioChecked = true;
+					tempPositon = position;
+				}
+			});
 			return convertView;
 		}
 	}
---
+++ src/com/pCheck/util/ViewHolder.java
@@ -0,0 +1,11 @@
+package com.pCheck.util;
+
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public final class ViewHolder {
+	public TextView app_text;
+	public ImageView app_img;
+	public RadioButton app_radio;
+}
